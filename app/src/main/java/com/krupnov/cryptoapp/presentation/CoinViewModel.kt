92ba32a78c6286f3d0ea19d46602b94d081336ee
Mikhail Krupnov <krupnovmn@gmail.com>
2023-11-19 00:49:08 +0300

    add dagger dependencies

--- app/src/main/java/com/krupnov/cryptoapp/presentation/CoinViewModel.kt
+++ app/src/main/java/com/krupnov/cryptoapp/presentation/CoinViewModel.kt
@@ -2,14 +2,10 @@ package com.krupnov.cryptoapp.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
-import com.krupnov.cryptoapp.data.network.model.CoinInfoDto
 import com.krupnov.cryptoapp.data.repository.CoinRepositoryImpl
 import com.krupnov.cryptoapp.domain.GetCoinInfoListUseCase
 import com.krupnov.cryptoapp.domain.GetCoinInfoUseCase
 import com.krupnov.cryptoapp.domain.LoadDataUseCase
-import kotlinx.coroutines.launch
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
