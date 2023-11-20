26dcdfb765148ff705008143c8f68f767a997255
Mikhail Krupnov <krupnovmn@gmail.com>
2023-11-21 00:04:27 +0300

    add inject multiple workers

--- app/src/main/java/com/krupnov/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/krupnov/cryptoapp/presentation/CoinApp.kt
@@ -2,17 +2,14 @@ package com.krupnov.cryptoapp.presentation
 
 import android.app.Application
 import androidx.work.Configuration
-import com.krupnov.cryptoapp.data.database.AppDatabase
-import com.krupnov.cryptoapp.data.mapper.CoinMapper
-import com.krupnov.cryptoapp.data.network.ApiFactory
-import com.krupnov.cryptoapp.data.workers.RefreshDataWorkerFactory
+import com.krupnov.cryptoapp.data.workers.CoinWorkerFactory
 import com.krupnov.cryptoapp.di.DaggerApplicationComponent
 import javax.inject.Inject
 
 class CoinApp : Application(), Configuration.Provider {
 
     @Inject
-    lateinit var workerFactory: RefreshDataWorkerFactory
+    lateinit var workerFactory: CoinWorkerFactory
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
